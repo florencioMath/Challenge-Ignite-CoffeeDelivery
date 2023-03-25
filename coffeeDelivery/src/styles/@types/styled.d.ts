80b01526330406cd29a70d81ad3d1095c2713cb3
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-03-24 21:57:34 -0300

    feat: Global style, default style and color variables.

---
+++ coffeeDelivery/src/styles/@types/styled.d.ts
@@ -0,0 +1,9 @@
+import { defaultTheme } from './../themes/default';
+import 'styled-components';
+import { defaultTheme } from '../themes/default';
+
+type ThemeType = typeof defaultTheme;
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
