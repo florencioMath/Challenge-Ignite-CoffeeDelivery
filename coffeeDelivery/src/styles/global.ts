5f8e47406fd99c01890534ed81df7307e980dd18
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-03-27 19:52:04 -0300

    feat: Global style.

--- coffeeDelivery/src/styles/global.ts
+++ coffeeDelivery/src/styles/global.ts
@@ -9,6 +9,27 @@ export const GlobalStyle = createGlobalStyle`
 
 body {
   background: ${(props) => props.theme['background']};
+  color: ${(props) => props.theme['base-text']};
+  -webkit-font-smoothing: antialiased;
+  padding: 2rem;
+}
+
+body, input, textarea, button {
+  font-family: 'Roboto' sans-serif;
+  font-weight: 400;
+  font-size: 1rem;
+}
+
+h1 {
+  font-family: 'Baloo', sans-serif;
+  font-weight: 400;
+  font-size: 1.125rem;
+}
+
+@media (max-width: 768px) {
+  html {
+    font-size: 87.5%
+  }
 }
 
 `;
