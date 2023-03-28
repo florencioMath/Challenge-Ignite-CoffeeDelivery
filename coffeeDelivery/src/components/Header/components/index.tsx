ef101613c276a351b6054ba5eb941c373f565970
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-03-27 22:09:00 -0300

    feat: Link to Cart component and  Header initial layout.

--- coffeeDelivery/src/components/Header/components/index.tsx
+++ coffeeDelivery/src/components/Header/components/index.tsx
@@ -1,12 +1,15 @@
 import { ShoppingCart } from '@phosphor-icons/react';
+import { Link } from 'react-router-dom';
 import { CartAmount, ShoppingCartWrapper } from './styles';
 
 export function Cart() {
   const cartTotal: number = 0;
   return (
-    <ShoppingCartWrapper>
-      <ShoppingCart size={22} weight="fill" />
-      {cartTotal > 0 ? <CartAmount>{cartTotal}</CartAmount> : null}
-    </ShoppingCartWrapper>
+    <Link to={'/checkout'}>
+      <ShoppingCartWrapper>
+        <ShoppingCart size={22} weight="fill" />
+        {cartTotal > 0 ? <CartAmount>{cartTotal}</CartAmount> : null}
+      </ShoppingCartWrapper>
+    </Link>
   );
 }
--- coffeeDelivery/src/components/Header/index.tsx
+++ coffeeDelivery/src/components/Header/index.tsx
@@ -1,7 +1,33 @@
+import { MapPin } from '@phosphor-icons/react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import { Cart } from './components';
+import {
+  Icon,
+  LocaleWrapper,
+  LogoAndThemeModeContainer,
+  NavBar,
+} from './styles';
+
 export function Header() {
   return (
-    <>
-      <h1>This is the Header</h1>
-    </>
+    <NavBar>
+      <LogoAndThemeModeContainer>
+        <Link to={'/'}>
+          <img src={logo} alt="" />
+        </Link>
+        <div>[DarkMode]</div>
+      </LogoAndThemeModeContainer>
+
+      <div>
+        <LocaleWrapper>
+          <Icon>
+            <MapPin size={22} weight="fill" />
+          </Icon>
+          <p>Balneário Camboriú, SC</p>
+        </LocaleWrapper>
+        <Cart />
+      </div>
+    </NavBar>
   );
 }
