dd332f2a0cad97dc5dc3b0430e2cf3bf0cf8b418
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-03-29 21:50:34 -0300

    feat: Change app themes and use the new context on App

--- coffeeDelivery/src/App.tsx
+++ coffeeDelivery/src/App.tsx
@@ -1,40 +1,19 @@
-import { createContext, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import { DefaultLayout } from './layouts/DefaultLayout';
 import { Router } from './Router';
+
+import { CustomThemeProvider } from './contexts/ThemeContext';
 import { GlobalStyle } from './styles/global';
-import { darkTheme } from './styles/themes/dark';
-import { lightTheme } from './styles/themes/light';
-
-type Theme = 'light' | 'dark';
-
-interface ThemeContextType {
-  currentTheme: Theme;
-  toggleTheme: () => void;
-}
-
-export const ThemeContext = createContext({} as ThemeContextType);
 
 export function App() {
-  const [currentTheme, setCurrentTheme] = useState<Theme>('dark');
-
-  function toggleTheme() {
-    currentTheme === 'dark'
-      ? setCurrentTheme('light')
-      : setCurrentTheme('dark');
-  }
   return (
     <>
-      <ThemeProvider theme={currentTheme === 'dark' ? darkTheme : lightTheme}>
-        <ThemeContext.Provider value={{ currentTheme, toggleTheme }}>
-          <BrowserRouter>
-            <Router />
-          </BrowserRouter>
+      <CustomThemeProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
 
-          <GlobalStyle />
-        </ThemeContext.Provider>
-      </ThemeProvider>
+        <GlobalStyle />
+      </CustomThemeProvider>
     </>
   );
 }
