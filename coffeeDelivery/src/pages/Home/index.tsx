e2621d8007de0e605f3710608bba2bdb1d00ef05
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-03-30 22:02:13 -0300

    feat: Some style and fix look of the home page.

--- coffeeDelivery/src/pages/Home/index.tsx
+++ coffeeDelivery/src/pages/Home/index.tsx
@@ -1,9 +1,57 @@
-import { HomeContainer } from './styels';
+import { AboutContainer, ContentContainer, HomeContainer } from './styels';
+
+import heroLogo from './assets/hero-logo.png';
+import { Coffee, Package, ShoppingCart, Timer } from '@phosphor-icons/react';
 
 export function Home() {
   return (
     <>
-      <HomeContainer>This is the Home</HomeContainer>
+      <HomeContainer>
+        <div>
+          <div>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </p>
+          </div>
+
+          <AboutContainer>
+            <ContentContainer>
+              <div>
+                <span>
+                  <ShoppingCart size={16} weight="fill" />
+                </span>
+                <p>Compra simples e segura</p>
+              </div>
+              <div>
+                <span>
+                  <Package size={16} weight="fill" />
+                </span>
+                <p>Embalagem mantém o café intacto</p>
+              </div>
+            </ContentContainer>
+            <ContentContainer>
+              <div>
+                <span>
+                  <Timer size={16} weight="fill" />
+                </span>
+                <p>Entrega rápida e rastreada</p>
+              </div>
+              <div>
+                <span>
+                  <Coffee size={16} weight="fill" />
+                </span>
+                <p>O café chega fresquinho até você</p>
+              </div>
+            </ContentContainer>
+          </AboutContainer>
+        </div>
+
+        <div>
+          <img src={heroLogo} alt="" />
+        </div>
+      </HomeContainer>
     </>
   );
 }
--- coffeeDelivery/src/pages/Home/styels.ts
+++ coffeeDelivery/src/pages/Home/styels.ts
@@ -2,4 +2,51 @@ import styled from 'styled-components';
 
 export const HomeContainer = styled.div`
   margin-top: 104px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    color: ${(props) => props.theme['base-title']};
+    font-size: 3rem;
+    max-width: 600px;
+  }
+
+  p {
+    color: ${(props) => props.theme['base-subtitle']};
+    font-size: 1.25rem;
+  }
+`;
+
+export const AboutContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  align-items: center;
+  margin-top: 4.125rem;
+
+  p {
+    color: ${(props) => props.theme['base-subtitle']};
+    font-size: 1rem;
+  }
+
+  span {
+    background: lightgray;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 32px;
+    min-width: 32px;
+    border-radius: 9999px;
+    color: ${(props) => props.theme['white']};
+  }
+`;
+
+export const ContentContainer = styled.div`
+  div {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    margin-bottom: 1.25rem;
+  }
 `;
