ed0a221eaa55c135b64e318a80ed9bb96fdfa36b
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-04-01 21:56:28 -0300

    fix: Name correction

---
+++ coffeeDelivery/src/pages/Home/components/Coffees/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const CoffeesContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  max-width: calc(100vw - 20rem);
+  width: 1440px;
+  padding: 2rem 10rem;
+  height: 100%;
+`;
