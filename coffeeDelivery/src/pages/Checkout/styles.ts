38434b24d5aa001411ee363a193f8591b0c95c8a
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-04-06 21:31:12 -0300

    feat: Inputs of the order.

--- coffeeDelivery/src/pages/Checkout/styles.ts
+++ coffeeDelivery/src/pages/Checkout/styles.ts
@@ -30,7 +30,92 @@ export const FinishOrderFormCard = styled.div`
   background: ${(props) => props.theme['base-card']};
   border-radius: 6px;
   width: 100%;
-  height: 300px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 2.5rem;
+
+  input {
+    &#cep {
+      max-width: 200px;
+    }
+
+    &#numero {
+      max-width: 200px;
+    }
+  }
+`;
+
+export const InputLabelContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+  position: relative;
+
+  input {
+    &#complemento {
+      max-width: 348px;
+      width: 100%;
+      position: relative;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+  }
+
+  #labelComplemento {
+    max-width: 340px;
+    width: 100%;
+    position: absolute;
+    font-style: italic;
+    margin-left: 75%;
+    color: ${(props) => props.theme['base-label']};
+  }
+`;
+
+export const InputNumeroComplementoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  column-gap: 0.75rem;
+`;
+
+export const InputAddressContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  column-gap: 0.75rem;
+
+  input {
+    &#bairro {
+      width: 200px;
+    }
+
+    &#cidade {
+      width: 276px;
+    }
+
+    &#uf {
+      width: 60px;
+    }
+  }
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 0.688rem;
+  margin-bottom: 2rem;
+`;
+
+export const IconMapPin = styled.div`
+  color: ${(props) => props.theme['yellow-dark']};
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const FinishOrderPaymentCard = styled.div`
@@ -45,6 +130,7 @@ export const SelectedCaffeesContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  gap: 10px;
 `;
 
 export const SelectedCaffeesCard = styled.div`
