66db19785fa2b7de1beb2e33a465ca97ccd3e087
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-04-12 20:54:21 -0300

    fix: Correction on cartItemsTotal.

--- coffeeDelivery/src/contexts/CartContext.tsx
+++ coffeeDelivery/src/contexts/CartContext.tsx
@@ -44,7 +44,7 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
   });
   const cartQuantity = cartItems.length;
   const cartItemsTotal = cartItems.reduce((total, cartItem) => {
-    return (total = cartItem.price + cartItem.quantity);
+    return total + cartItem.price * cartItem.quantity;
   }, 0);
 
   function addCoffeeToCart(coffee: CartItem) {
